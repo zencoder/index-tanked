ffe6e34be3b5ed83983b578a4121c18a4031eb57
Adam Kittelson <adam@zencoder.com>
2011-02-11 09:56:44 -0600

    configurable doc_id, field attributes and what goes into the text field

--- lib/index-tanked/class_methods.rb
+++ lib/index-tanked/class_methods.rb
@@ -1,14 +1,24 @@
 module IndexTanked
   module ClassMethods
-    attr_reader :tanked_fields, :index
+    attr_reader :index_tanked_fields, :index_tanked_text, :index_tanked_index, :index_tanked_doc_id
 
     def index_tanked(index=nil, &block)
-      @index = index || IndexTanked::Configuration.index
+      @index_tanked_index = index || IndexTanked::Configuration.index
       self.instance_exec &block
     end
 
-    def tanks(field, method=nil)
-      @tanked_fields <<  [field, method]
+    def doc_id(method)
+      @index_tanked_doc_id = method
+    end
+
+    def field(field_name, method=field_name, options = {})
+      method, options = field_name, method if method.is_a? Hash
+      @index_tanked_fields <<  [field_name, method, options]
+    end
+
+    def text(method)
+      @index_tanked_text << method
     end
   end
+
 end
--- lib/index-tanked/index_tanked.rb
+++ lib/index-tanked/index_tanked.rb
@@ -1,20 +1,32 @@
 module IndexTanked
 
   def self.included(klass)
-    klass.instance_variable_set(:@tanked_fields, [])
+    klass.instance_variable_set(:@index_tanked_fields, [])
+    klass.instance_variable_set(:@index_tanked_text, [])
     klass.extend ClassMethods
   end
 
   def add_to_index_tank
-    data = self.class.tanked_fields.inject({}) do |data, (field, method)|
-      data[field] = method ? method.call(self) : send(field)
-      data
-    end
-    data.merge!(:text => data.values.join(" "))
-    data.merge!(:model => self.class.name)
-    data.merge!(:timestamp => created_at.to_i)
+    field_data = {}
 
-    index_tank_index.document("#{self.class.name}: #{id}").add(data)
+    text_values = self.class.index_tanked_text.map { |method| get_value_from_method_or_proc method }
+
+    self.class.index_tanked_fields.each do |(field, method, options)|
+      field_data[field] = get_value_from_method_or_proc method
+      text_value = options.has_key?(:text) ? get_value_from_method_or_proc(options[:text]) : field_data[field]
+      text_values << text_value
+    end
+
+    field_data.merge!(:text => text_values.compact.uniq.join(" "))
+
+    field_data.merge!(:model => self.class.name)
+    field_data.merge!(:timestamp => created_at.to_i)
+
+    index_tank_index.document(index_tank_doc_id).add(field_data)
+  end
+
+  def index_tank_doc_id
+    get_value_from_method_or_proc(self.class.index_tanked_doc_id) || "#{self.class.name}: #{id}"
   end
 
 protected
@@ -24,7 +36,16 @@ protected
   end
 
   def index_tank_index
-    @index_tank_index ||= index_tank_api_client.indexes self.class.index
+    @index_tank_index ||= index_tank_api_client.indexes self.class.index_tanked_index
+  end
+
+  def get_value_from_method_or_proc(method)
+    case method
+    when Symbol
+      self.send method
+    when Proc
+      method.call(self)
+    end
   end
 
 end
