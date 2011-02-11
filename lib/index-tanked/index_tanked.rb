cd361275897c702166db6fa4b9e32476e3bdac41
Adam Kittelson <adam@zencoder.com>
2011-02-11 14:10:17 -0600

    fixed a bug, made a helpful commit message

--- lib/index-tanked/index_tanked.rb
+++ lib/index-tanked/index_tanked.rb
@@ -9,12 +9,11 @@ module IndexTanked
   def add_to_index_tank
     field_data = {}
 
-    text_values = self.class.index_tanked_text.map { |method| get_value_from_method_or_proc method }
+    text_values = self.class.index_tanked_text.map { |method| get_value_from method }
 
     self.class.index_tanked_fields.each do |(field, method, options)|
-      field_data[field] = get_value_from_method_or_proc method
-      text_value = options.has_key?(:text) ? get_value_from_method_or_proc(options[:text]) : field_data[field]
-      text_values << text_value
+      field_data[field] = get_value_from method
+      text_values << (options.has_key?(:text) ? get_value_from(options[:text]) : field_data[field])
     end
 
     field_data.merge!(:text => text_values.compact.uniq.join(" "))
@@ -26,7 +25,7 @@ module IndexTanked
   end
 
   def index_tank_doc_id
-    get_value_from_method_or_proc(self.class.index_tanked_doc_id) || "#{self.class.name}: #{id}"
+    get_value_from(self.class.index_tanked_doc_id) || "#{self.class.name}:#{id}"
   end
 
 protected
@@ -39,12 +38,14 @@ protected
     @index_tank_index ||= index_tank_api_client.indexes self.class.index_tanked_index
   end
 
-  def get_value_from_method_or_proc(method)
+  def get_value_from(method)
     case method
     when Symbol
       self.send method
     when Proc
       method.call(self)
+    else
+      method
     end
   end
 
