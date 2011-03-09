f081d6c21d2d0fad698afcfe8114db3c1108728b
Adam Kittelson <adam@zencoder.com>
2011-03-09 09:55:03 -0600

    added availability to configuration

--- lib/index-tanked/configuration.rb
+++ lib/index-tanked/configuration.rb
@@ -3,7 +3,15 @@ module IndexTanked
   class Configuration
 
     class << self
-      attr_accessor :url, :index
+      attr_accessor :url, :index, :availability
+
+      def available?
+        if availability.is_a? Proc
+          availability.call
+        else
+          availability
+        end
+      end
     end
 
   end
