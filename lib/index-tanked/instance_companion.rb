f97b9715abb7ba0fd75a0a1145448c74ccfc9f66
Adam Kittelson <adam@zencoder.com>
2011-03-15 16:26:45 -0500

    clean up and test instance companion

--- lib/index-tanked/instance_companion.rb
+++ lib/index-tanked/instance_companion.rb
@@ -3,45 +3,45 @@ module IndexTanked
 
     def initialize(companion)
       @companion = companion
+      @index_tanked = companion.class.index_tanked
     end
 
     def data
       field_data = {}
       other_data = {}
 
-      text_values = @companion.class.index_tanked.texts.map { |method| @companion.class.index_tanked.get_value_from(@companion, method) }
+      text_values = @index_tanked.texts.map { |method| @index_tanked.get_value_from(@companion, method) }
 
-      @companion.class.index_tanked.fields.each do |(field, method, options)|
-        value = @companion.class.index_tanked.get_value_from(@companion, method)
+      @index_tanked.fields.each do |(field, method, options)|
+        value = @index_tanked.get_value_from(@companion, method)
         unless value.nil?
           field_data[field] = value
-          text_values << (options.has_key?(:text) ? @companion.class.index_tanked.get_value_from(@companion, options[:text]) : field_data[field])
+          text_values << (options.has_key?(:text) ? @index_tanked.get_value_from(@companion, options[:text]) : field_data[field])
         end
       end
 
       field_data.merge!(:text => text_values.compact.uniq.join(" "))
 
-      variables = @companion.class.index_tanked.variables.inject({}) do |variables, (variable, method)|
-        variables[variable] = @companion.class.index_tanked.get_value_from(@companion, method)
+      variables = @index_tanked.variables.inject({}) do |variables, (variable, method)|
+        variables[variable] = @index_tanked.get_value_from(@companion, method)
         variables
       end
 
       other_data[:variables] = variables unless variables.empty?
 
       [field_data, other_data]
-
     end
 
     def doc_id
-      @companion.class.index_tanked.get_value_from(@companion, @companion.class.index_tanked.doc_id_value)
+      @index_tanked.get_value_from(@companion, @index_tanked.doc_id_value)
     end
 
     def api_client
-      @index_tank_api_client ||= @companion.class.index_tanked.api_client
+      @index_tanked.api_client
     end
 
     def index
-      @index_tank_index ||= @companion.class.index_tanked.index
+      @index_tanked.index
     end
 
   end
---
+++ test/unit/instance_companion_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+module IndexTanked
+
+  class InstanceCompanionTest < Test::Unit::TestCase
+    context "An instance of a class with index-tanked included" do
+      setup do
+        class AnimalsRawr
+          include IndexTanked
+
+          attr_accessor :fish, :dog
+
+          def initialize(opts={})
+            @fish = opts[:fish]
+            @dog = opts[:dog]
+          end
+
+          index_tank :index => 'animals', :url => "http://example.com" do
+            doc_id lambda { |instance| "animals/fish/#{instance.fish}/dog/#{instance.dog}" }
+            field  :dog
+            field  :fish, :text => nil
+            text   "animals fish dogs"
+            var 0, 42
+          end
+        end
+
+        @instance = AnimalsRawr.new({:fish => 'trout', :dog => 'shiba inu'})
+      end
+
+      teardown do
+        AnimalsRawr.index_tanked.fields.clear
+        AnimalsRawr.index_tanked.texts.clear
+        AnimalsRawr.index_tanked.variables.clear
+      end
+
+      should "have an index_tanked companion object" do
+        assert @instance.index_tanked.is_a? IndexTanked::InstanceCompanion
+      end
+
+      context "the companion" do
+        setup do
+          @companion = @instance.index_tanked
+        end
+
+        should "serialize its data for adding to index tank" do
+          assert_equal [{:text => "animals fish dogs shiba inu",
+                         :fish => "trout",
+                         :dog => "shiba inu"},
+                        {:variables => { 0 => 42}}], @companion.data
+        end
+
+        should "generate a doc_id for the instance" do
+          assert_equal "animals/fish/trout/dog/shiba inu", @companion.doc_id
+        end
+
+        should "access its classes' index" do
+          assert @companion.index.is_a? IndexTank::Index
+        end
+
+        should "return an IndexTank::Client" do
+          assert @companion.api_client.is_a? IndexTank::Client
+        end
+      end
+    end
+  end
+end
