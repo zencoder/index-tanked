bffbf4f8d8c73e2ada7918a4616d963431c45946
Adam Kittelson <adam@zencoder.com>
2011-05-17 10:43:50 -0500

    add model

--- lib/generators/index_tanked/templates/migration.rb
+++ lib/generators/index_tanked/templates/migration.rb
@@ -2,6 +2,7 @@ class CreateIndexTankedDocuments < ActiveRecord::Migration
   def self.up
     create_table :index_tanked_documents, :force => true do |t|
       t.string   :docid                        # IndexTank docid of the document
+      t.string   :model                        # Model so we can query it for indextank url / index name
       t.text     :document                     # document from #document_for_batch_addition
       t.datetime :locked_at                    # Set when a client is working on this object
       t.string   :locked_by                    # Who is working on this object (if locked)
