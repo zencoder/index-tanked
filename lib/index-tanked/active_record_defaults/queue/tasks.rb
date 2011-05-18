cc3f1bdf0932588664b54adc7b23e486e05365d0
Adam Kittelson <adam@zencoder.com>
2011-05-18 14:43:48 -0500

    argument was on the wrong method

--- lib/index-tanked/active_record_defaults/queue/tasks.rb
+++ lib/index-tanked/active_record_defaults/queue/tasks.rb
@@ -2,7 +2,7 @@ namespace :indextanked do
   namespace :queue do
     desc "Start an index-tanked queue worker."
     task :process => :environment do
-      IndexTanked::ActiveRecordDefaults::Queue::Worker.new.start(:batch_size => ENV['BATCH_SIZE'])
+      IndexTanked::ActiveRecordDefaults::Queue::Worker.new(:batch_size => ENV['BATCH_SIZE']).start
     end
   end
 end
