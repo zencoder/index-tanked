e5e8cc075f8a3fc73e0e904f9fd840feb4415d5d
Adam Kittelson <adam@zencoder.com>
2011-05-20 09:41:42 -0500

    added an index

--- lib/generators/index_tanked/templates/migration.rb
+++ lib/generators/index_tanked/templates/migration.rb
@@ -6,12 +6,14 @@ class CreateIndexTankedDocuments < ActiveRecord::Migration
       t.text     :document                     # document from #document_for_batch_addition
       t.datetime :locked_at                    # Set when a client is working on this object
       t.string   :locked_by                    # Who is working on this object (if locked)
-
       t.timestamps
     end
+
+    add_index :index_tanked_documents, :locked_at
   end
 
   def self.down
+    remove_index :index_tanked_documents, :locked_at
     drop_table :index_tanked_documents
   end
 end
