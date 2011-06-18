8ca6d0c287ec89838428982a769df4557cc26788
Adam Kittelson <adam@zencoder.com>
2011-06-17 17:33:40 -0700

    test deleting outdated locked records

--- test/unit/active_record_defaults/document_test.rb
+++ test/unit/active_record_defaults/document_test.rb
@@ -118,6 +118,45 @@ module IndexTanked
           end
         end
 
+        context "Three locked records" do
+          context "one with an unlocked model_name / record_id twin that is newer than itself" do
+            setup do
+              @locked_person_old = Document.enqueue(1, 'Person', {})
+              @locked_person_old.update_attributes(:locked_by => 'testing', :locked_at => Time.now)
+              @unlocked_person_new = Document.enqueue(1, 'Person', {})
+            end
+            context "one with an unlocked model_name / record_id twin that is older than itself" do
+              setup do
+                @unlocked_robot_old = Document.enqueue(6, 'Robot', {})
+                @locked_robot_new = Document.create(:record_id => 6,
+                                                    :model_name =>  'Robot',
+                                                    :document => {},
+                                                    :locked_by => 'testing',
+                                                    :locked_at => Time.now)
+              end
+              context "and one that is unique" do
+                setup do
+                  @beautiful_snowflake = Document.enqueue(3, 'Snowflake', {})
+                  @beautiful_snowflake.update_attributes(:locked_by => 'testing', :locked_at => Time.now)
+                end
+
+                should "be able to find the non unique combinations records locked for testing" do
+                  assert_same_elements [["Person", 1], ["Robot", 6]], Document.non_unique_docids_by_identifier('testing')
+                end
+
+                should "be able to delete the locked record that is outdated" do
+                  person_old_id = @locked_person_old.id
+                  assert_equal 1, Document.delete_outdated_locked_records_by_identifier('testing')
+                  exception = assert_raises ActiveRecord::RecordNotFound do
+                    @locked_person_old.reload
+                  end
+                  assert_equal "Couldn't find IndexTanked::ActiveRecordDefaults::Queue::Document with ID=#{person_old_id}", exception.message
+                end
+
+              end
+            end
+          end
+        end
       end
     end
   end
